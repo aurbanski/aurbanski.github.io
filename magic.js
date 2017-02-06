850e522d5e840d70b9c43c6364eb3dc410173c8b
aurbanski <aurbanski27@gmail.com>
2017-02-06 16:54:44 -0700

    Added all files

---
+++ magic.js
@@ -0,0 +1,35 @@
+$('.button.first').on('click', function(e){
+  e.preventDefault();
+  $('.form').addClass('animate_out');
+  $arr = $('form').serializeArray();
+  $arr2 = [$arr.length];
+  $.each($arr, function(i, field){
+    $arr2[i] = field.value;
+  });
+
+  $cwp = $arr2[0] * $arr2[1] * $arr2[2] * 7;
+  $cmp = $arr2[0] * $arr2[1] * $arr2[2] * 30;
+  $cap = $arr2[0] * $arr2[1] * $arr2[2] * 365;
+
+  $owp = $arr2[0] * $arr2[1] * $arr2[2] * 7 / 2 ;
+  $omp = $arr2[0] * $arr2[1] * $arr2[2] * 30 / 2;
+  $oap = $arr2[0] * $arr2[1] * $arr2[2] * 365 / 2;
+
+  $swp = $cwp - $owp;
+  $smp = $cmp - $omp;
+  $sap = $cap - $oap;
+
+  setTimeout(function(){
+    $('.cwp').html("You pay: $" + $cwp);
+    $('.owp').html("Our cost: $" + $owp);
+    $('.swp').html("Potential Savings: $" + $swp);
+    $('.cmp').html("You pay: $" + $cmp);
+    $('.omp').html("Our cost: $" + $omp);
+    $('.smp').html("Potential Savings: $" + $smp);
+    $('.cap').html("You pay: $" + $cap);
+    $('.oap').html("Our cost: $" + $oap);
+    $('.sap').html("Potential Savings: $" + $sap);
+    $('.form').removeClass('is_showing');
+    $('.results').addClass('animate_in');
+  }, 300);
+});
